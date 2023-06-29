15ac2f27b86ee6a7de294c634fcd15b9f29f7f29
Ofir Shapira <48770263+sharp30@users.noreply.github.com>
2023-06-29 20:05:14 +0300

    added structure

--- MoveVehicle.h
+++ MoveVehicle.h
@@ -7,7 +7,8 @@
 
 #include "CellType.h"
 #include "Direction.h"
-
+#include "BoardCell.h"
+#include "List.h"
 
 template<CellType cell,Direction dir,int N>
 struct Move
@@ -18,23 +19,68 @@ struct Move
     static constexpr int amount = N;
 };
 
+template <typename Board,int R,int C>
+struct ValidIndex
+{
+
+    static_assert(R >=0 && R < Board::width, "Invalid Index");
+    static_assert(C >=0 && C < Board::length, "Invalid Index");
+
+    typedef GetAtIndex<R,typename Board::board> row;
+    typedef GetAtIndex<C, row> cell;
+    static_assert(cell::type == EMPTY, "Invalid Index");
+
+};
+
+template <typename Board,int R,int C>
+struct GetLeft{
+    typedef
+};
+
 
 //struct get_Left ->
 //struct get_right ->
 //struct perform move
 //struct valid index
+//struct get cell
+
+
 /*
-template<typename ,int ,int,Direction,int >
+template<typename ,int ,int,Direction,int > - Done
 struct MoveVehicle
 {
-
 };*/
 
-template<typename board,int R ,int C,Direction D,int A>
+
+template <typename Board,int R,int C>
+struct GetCellAtIndex
+{
+private:
+    typedef GetAtIndex<R,typename Board::board> row;
+public:
+    typedef GetAtIndex<C, row> cell;
+};
+
+
+template <typename Board,int R,int C,Direction D,int A>
+struct PerformMoves{};
+
+
+template <typename Board,int R,int C,Direction D>
+struct PerformMoves<Board,R,C,D,0>
+{
+    typedef Board:
+};
+template<typename Board,int R ,int C,Direction D,int A>
 struct MoveVehicle
 {
-    static_assert(board::);
-};
+    //do step A times
+    static_assert(Board::board::get);/
+    //do checking
+    //handle cases rows vs
+    //make moves <board,edge,direction,amount>
+
+}
 
 
 
