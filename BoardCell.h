2ee2631bb6750f06e58f7e81d2fc99c9617bb1a0
yonatanelbaz <yonatanelbaz22@gmail.com>
2023-06-29 18:53:19 +0300

    BoardCell and GameBoard

---
+++ BoardCell.h
@@ -0,0 +1,11 @@
+#ifndef OOP5_BOARDCELL_H
+#define OOP5_BOARDCELL_H
+
+template<typename T, typename T2, typename T3>
+struct BoardCell{
+    typedef T type;
+    typedef T2 direction;
+    typedef T3 length;
+
+};
+#endif //OOP5_BOARDCELL_H
